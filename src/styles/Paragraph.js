5a3a52b60f915bef28b1bf0e28554cdc2fbb59c5
Ash <ashley.healy@codeclan.com>
2023-07-06 16:19:46 +0100

    add

---
+++ src/styles/Paragraph.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Paragraph = styled.p`
+    font-family: Arial, Helvetica, sans-serif;
+    margin:0;
+    padding:1em;
+    background-color: #ced7e0;
+    color: #062c43;
+`;
+
+export default Paragraph;
